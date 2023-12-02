7fb1eb4686fea847695eff346b945e9037b93c4f
seunghabaek <slug0326@naver.com>
2023-12-02 16:33:18 +0900

    add api code

--- routers/books.py
+++ routers/books.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header
+from starlette.config import Config
+
+# setup config
+config = Config('.env')
+
+def get_naver_headers(
+    x_naver_client_id: str = Header(...),
+    naver_client_secret: str = Header(...),
+):
+    return {
+        "naver_client_id": x_naver_client_id,
+        "naver_client_secret": naver_client_secret,
+    }
 
 router = APIRouter(
-	prefix="/books",
+    prefix="/books",
     tags=["books"],
-    dependencies=[Depends()],
+    dependencies=[Depends(get_naver_headers)],
 )
 
 @router.get("/get_book_list")
-def get_book_list() -> dict[str, str]:
-    return {"message": "Hello World"}
-    
+def get_book_list(headers: dict = Depends(get_naver_headers)) -> dict[str, str]:
+    return {"message": "Hello World", "headers": headers} # type: ignore
