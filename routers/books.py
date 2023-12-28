aa1aeb11064a8ee6642dd180b3e9f9a1bcaad5a2
seunghabaek <slug0326@naver.com>
2023-12-28 23:00:46 +0900

    delete description

--- routers/books.py
+++ routers/books.py
@@ -12,8 +12,10 @@ load_dotenv()
 X_NAVER_CLIENT_ID = os.getenv("X_NAVER_CLIENT_ID")
 X_NAVER_CLIENT_SECRET = os.getenv("X_NAVER_CLIENT_SECRET")
 
-header_dict = {"X-Naver-Client-Id": X_NAVER_CLIENT_ID, "X-Naver-Client-Secret":X_NAVER_CLIENT_SECRET}
-
+header_dict = {
+    "X-Naver-Client-Id": X_NAVER_CLIENT_ID,
+    "X-Naver-Client-Secret": X_NAVER_CLIENT_SECRET
+}
 
 router = APIRouter(
     prefix="/books",
@@ -21,16 +23,11 @@ router = APIRouter(
     # dependencies=[Depends(get_naver_headers)],
 )
 
+
 @router.get("/get_book_list")
 async def get_book_list(keyword: str):
     display = 10
     sort = "sim"
     response = get_naver_books_list(keyword, display, sort, header_dict)
-    
-    return {"books" : response} # type: ignore
 
-
-@router.get("/")
-async def root():
-    return {"message": f"{X_NAVER_CLIENT_ID}"}
-
+    return {"books": response}  # type: ignore
