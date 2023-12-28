740341017f8407f96e19d05fd46f4f0a1b11a131
seunghabaek <slug0326@naver.com>
2023-12-28 22:53:26 +0900

    add api

--- domain/books/books_service.py
+++ domain/books/books_service.py
@@ -1,7 +1,11 @@
+import json
 import requests
 
-def get_naver_books_list(query: str, display: int, sort="sim"):
-    params = {'query': {query}, 'display': {display}, 'sort': {sort}}
+def get_naver_books_list(query: str, display: int, sort: str, header_dict:dict):
+    params = {'query': query, 'display': display, 'sort': sort}
     url = "https://openapi.naver.com/v1/search/book.json"
-    response = requests.get(url, params=params)
-    return response
+    
+    response = requests.get(url=url, params=params, headers=header_dict)
+    response_json = json.loads(response.content)
+
+    return response_json.get('items')
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI, HTTPException
+import json
+import os
+import requests
+
+from fastapi import FastAPI, HTTPException, Header, Depends
+from domain.books.books_service import get_naver_books_list
+# from domain.books.books_service import get_naver_books_list
 from models import ItemPayload
 from routers import books
 
+from dotenv import load_dotenv
 
 app = FastAPI()
 grocery_list: dict[int, ItemPayload] = {}
 
 app.include_router(books.router)
+
+load_dotenv()
+
+# import env file
+X_NAVER_CLIENT_ID = os.getenv("X_NAVER_CLIENT_ID")
+X_NAVER_CLIENT_SECRET = os.getenv("X_NAVER_CLIENT_SECRET")
+
+headers = {"X-Naver-Client-Id": X_NAVER_CLIENT_ID,
+    "X-Naver-Client-Secret": X_NAVER_CLIENT_SECRET}
+
+@app.get("/get_book_list_test")
+async def root():
+    response = get_naver_books_list(query="love", display=10, sort="sim", header_dict=headers)
+    return response
