1b4056f5e4c0f4a622e9f17b4225d94c8706657a
seunghabaek <slug0326@naver.com>
2023-12-28 23:47:31 +0900

    response form 변경

--- domain/books/books_service.py
+++ domain/books/books_service.py
@@ -1,11 +1,12 @@
 import json
 import requests
 
-def get_naver_books_list(query: str, display: int, sort: str, header_dict:dict):
+
+def get_naver_books_list(query: str, display: int, sort: str, header_dict: dict):
     params = {'query': query, 'display': display, 'sort': sort}
     url = "https://openapi.naver.com/v1/search/book.json"
-    
+
     response = requests.get(url=url, params=params, headers=header_dict)
     response_json = json.loads(response.content)
 
-    return response_json.get('items')
+    return response_json
