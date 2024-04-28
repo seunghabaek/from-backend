50906fe479efc31ea2045e9fda3d5df74762fa0d
seunghabaek <slug0326@naver.com>
2024-04-28 22:49:52 +0900

    fix: add api

--- app/main.py
+++ app/main.py
@@ -1,28 +1,48 @@
 import os
+from typing import Optional
+
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
-# from app.routers import books
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # app.include_router(books.router)
 
-@app.get("/")
-def read_root():
-    return {"test": "FastAPI Running"}
 
-@app.post("/write_book_review")
+# class BookItem(BaseModel):
+#     title: str
+#     book_cover: str
+#     author: str
+#     publisher: str
+#     publish_date: str
+#     isbn: str
+#     description: Optional[str] = None
+
+@app.get("/api/v1/get_book_list")
+async def get_book_list():
+    return {
+        "title":"test",
+        "book_cover":"http://www.naver.com",
+        "author":"seungha.baek",
+        "publish_date":"2024-05-03",
+        "isbn":"sdf11133",
+        "description":"이것은 테스트입니다.",
+    }
+
+
+@app.post("/api/v1/write_book_review")
 async def write_book_review():
     # request body
     return {"result_type": "SUCCESS", "results:": "review done!"}
 
 
-@app.get("/get_book_review")
+@app.get("/api/v1/get_book_review")
 async def get_book_review():
     return {"result_type": "SUCCESS", "results:": "review done!"}
 
 
-@app.get("/get_reviewer_review_list")
+@app.get("/api/v1/get_reviewer_review_list")
 async def get_reviewer_review_list():
     return {"result_type": "SUCCESS", "results:": "review done!"}
 
